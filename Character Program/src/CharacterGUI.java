5b42729d065a17a301e3135ebfa57c8b820f2504
Michael Reeve <michael.s.reeve@gmail.com>
2019-11-09 15:01:41 -0500

    working version without tab support

--- Character Program/src/CharacterGUI.java
+++ Character Program/src/CharacterGUI.java
@@ -1,53 +1,31 @@
-public class CharacterGUI {
+import BreezySwing.*;
+import javax.swing.*;
+
+public class CharacterGUI extends GBFrame{
+	
+	JTextField inputField = addTextField("                     And     he   came 2   school.....??''',,,,...",1,1,1,1);
+	JButton enterButton = addButton("Enter",2,1,1,1);
+	JTextArea resultArea = addTextArea("",3,1,1,1);
+	
+	public void buttonClicked(JButton button) {
+		if(button == enterButton) {
+			String input = inputField.getText();
+			if(input.length() == 0) {
+				resultArea.setText("Error: empty input");
+				return;
+			}
+			StringParser p = new StringParser(inputField.getText());
+			p.generate();
+			resultArea.setText(p.toString());
+		}
+	}
+	
 	public static void main(String[] args) {
-		String test = "                     And     he     came 2   school.";
+		CharacterGUI frm = new CharacterGUI();
+		frm.setTitle("Character Program");
+		frm.setSize(400,400);
+		frm.setVisible(true);
 		
-		String correctSpaces = "";
-		
-		char[] split = toCharArrayCool(test);
-		for(int i = 0; i < split.length; i++) {
-			while(i < split.length -1 && !(split[i] == ' ' && split[i+1] == ' ')) {
-				correctSpaces += split[i];
-				i++;
-			}
-			if(i == split.length-1 && split[i] != ' ') {
-				correctSpaces += split[i];
-			}
-		}
-		correctSpaces = correctSpaces.substring(1);
-		
-		System.out.println(correctSpaces);
-		
-		String[] test2 = correctSpaces.split(" ");
-		System.out.println(test2.length);
-		for(String s : test2) {
-			System.out.println("\"" + s + "\"");
-		}
-	}
-	
-	
-	public static char[] toCharArrayCool(String str) {
-		char[] result = new char[str.length()];
-		for(int i = 0; i < str.length(); i++) {
-			result[i] = str.charAt(i);
-		}
-		return result;
-	}
-	
-	public static String[] splitString(String str, char c) {
-		String[] result = new String[getAmountOfWords(str)];
-		for(int i = 0; i < str.length(); i++) {
-			String s = "";
-		}
-		return result;
-	}
-	
-	public static int getAmountOfWords(String str) {
-		int amount = 1;
-		for(int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == ' ')amount++;
-		}
-		return amount;
 	}
 	
 }
---
+++ Character Program/src/StringParser.java
@@ -0,0 +1,139 @@
+
+public class StringParser {
+
+	private String raw;
+	
+	private String[] strings;
+	
+	private int characterCount;
+	
+	public StringParser(String str) {
+		raw = str.toLowerCase();
+		characterCount = -1;
+	}
+	
+	public void generate() {
+		String correct = removeSpaces(raw);
+		strings = splitString(correct, ' ');
+		removeTrailingCharacters();
+	}
+	
+	private String removeSpaces(String str) {
+		String result = "";
+		char[] split = createCharArray(str);
+		determineCharacterCount(split);
+		for(int i = 0; i < split.length; i++) {
+			while(i < split.length -1 && !(isTabSpace(split[i]) && isTabSpace(split[i+1]))) {
+				result += split[i];
+				i++;
+			}
+			if(i == split.length-1 && !isTabSpace(split[i])) {
+				result += split[i];
+			}
+		}
+		if(isTabSpace(result.charAt(0))){
+			result = result.substring(1);
+		}
+		return result;
+	}
+	
+	private boolean isTabSpace(char c) {
+		if(c == '	' || c == ' ')return true;
+		else return false;
+	}
+	
+	private char[] createCharArray(String str) {
+		char[] result = new char[str.length()];
+		for(int i = 0; i < str.length(); i++) {
+			result[i] = str.charAt(i);
+		}
+		return result;
+	}
+	
+	private String[] splitString(String str, char c) {
+		String[] result = new String[getAmountOfWords(str)];
+		int count = 0;
+		for(int i = 0; i < str.length(); i++) {
+			String s = "";
+			while(i < str.length() && str.charAt(i) != c) {
+				s+=str.charAt(i);
+				i++;
+			}
+			if(s.length() > 0)result[count++] = s;
+		}
+		return result;
+	}
+	
+	public int getAmountOfWords(String str) {
+		int amount = 1;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ' ')amount++;
+		}
+		return amount;
+	}
+	
+	private void removeTrailingCharacters() {
+		char[] last = createCharArray(strings[strings.length-1]);
+		String lastStr = strings[strings.length-1];
+		for(int i = last.length-1; i > 0; i--) {
+			char c = last[i];
+			char h = last[i-1];
+			if(isPunctuation(c) && isPunctuation(h)) {
+				lastStr = lastStr.substring(0, lastStr.length()-1);
+			}
+		}
+		if(isPunctuation(strings[strings.length-1].charAt(strings[strings.length-1].length()-1))){
+			strings[strings.length-1] = lastStr.substring(0,lastStr.length()-1);
+		}
+		
+	}
+	
+	private boolean isPunctuation(char c) {
+		if(c == '.' || c == '?' || c == '!' || c == ',' || c == '\'' || c == ':' || c == ';')return true;
+		else return false;
+	}
+	
+	private void determineCharacterCount(char[] charArr) {
+		int count = 0;
+		for(char c : charArr) if(!Character.isWhitespace(c))count++;
+		characterCount = count;
+	}
+	
+	public String toString() {
+		String result = 
+				characterCount + " Characters\n" + 
+				strings.length + " Words\n";
+		for(int i = 0; i < strings.length; i++) {
+			String str = strings[i];
+			if(exists(strings,str,i))continue;
+			int occurences = amountOfOccurences(str);
+			makeFirstCharUpperCase();
+			result += strings[i] + " " + occurences + '\n';
+		}
+		return result;
+	}
+		
+	private boolean exists(String[] arr, String str, int start) {
+		boolean result = false;
+		for(int i = start-1; i >= 0; i--) {
+			if(arr[i].equals(str))return true;
+		}
+		return result;
+	}
+	
+	private int amountOfOccurences(String str) {
+		int occurences = 0;
+		for(String s : strings) {
+			if(s.equals(str))occurences++;
+		}
+		return occurences;
+	}
+	
+	private void makeFirstCharUpperCase() {
+		for(int i = 0; i < strings.length; i++) {
+			if(Character.isAlphabetic(strings[i].charAt(0))) {
+				strings[i] = Character.toUpperCase(strings[i].charAt(0)) + strings[i].substring(1);
+			}
+		}
+	}
+}
